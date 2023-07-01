2daa5c93ee744a0b0d89f0f333ddbcda47fba45e
luclagrange4973@gmail.com <luclagrange4973@gmail.com>
2023-07-01 16:32:07 +0000

    constructor_standings

---
+++ models/staging/stg_constructor_results.sql
@@ -0,0 +1,13 @@
+{{ config(
+    materialized='table',
+    cluster_by = "id_constructor",
+) }}
+
+SELECT
+    points AS points_gained
+    , CAST(constructorresultsid AS STRING) AS id_constructor_result
+    , CAST(constructorid AS STRING) AS id_constructor
+    , CAST(raceid AS STRING) AS id_race
+    , COALESCE (status = 'D', FALSE) AS bl_disqualified
+FROM
+    `formula-1-391319.Formula1.constructor_results`
---
+++ models/staging/stg_constructor_standings.sql
@@ -0,0 +1,14 @@
+{{ config(
+    materialized='table',
+    cluster_by = "id_circuit",
+) }}
+
+SELECT
+  CAST(constructorStandingsId AS STRING) AS id_constructor_standings
+  , CAST(raceid AS STRING) AS id_race
+  , CAST(constructorid AS STRING) AS id_constructor
+  , points AS points_gained
+  , position AS end_position
+  , wins AS wins_number
+FROM
+  `formula-1-391319.Formula1.constructor_standings`
