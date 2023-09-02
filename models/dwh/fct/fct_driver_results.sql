3a6dba5a9a38505646aee444d5db380755fd40f2
luclagrange4973@gmail.com <luclagrange4973@gmail.com>
2023-09-02 17:04:36 +0000

    driver_result

---
+++ models/dwh/fct/fct_driver_results.sql
@@ -0,0 +1,20 @@
+SELECT
+    re.id_result
+    , re.id_race
+    , re.id_driver
+    , re.final_rank
+    , d.driver_name
+    , ra.id_circuit
+    , ra.season_year
+    , ra.season_round
+FROM
+    {{ref('stg_results')}} AS re
+LEFT JOIN
+    {{ ref('stg_races') }} AS ra
+    ON
+        re.id_race = ra.id_race
+LEFT JOIN
+    {{ ref('dim_drivers') }} AS d
+    ON
+        re.id_driver = d.id_driver
+WHERE id_result !="resultId"
